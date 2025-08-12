3dbcec7ee2617af4bf6de2ba2e1e8cc73a51741c
Kevin Tsang <mail@ktsang.com>
2025-08-12 11:40:42 +0100

    add license config

--- packages/toppan-ui/src/config.ts
+++ packages/toppan-ui/src/config.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) Toppan Security. All rights reserved.
+ *
+ * This software and associated documentation files (the "Software") are proprietary
+ * to Toppan Security and are protected by copyright law and international treaties.
+ *
+ * Unauthorized reproduction, distribution, or use of this Software, in whole or in
+ * part, is strictly prohibited without the express written permission of Toppan Security.
+ */
+
 export const config = {
   TOPPAN_SERVICE_URL: import.meta.env.TOPPAN_SERVICE_URL || 'http://localhost:3888',
   APPLICATION_CONFIG_URL: import.meta.env.APPLICATION_CONFIG_URL || 'http://localhost:2021'
