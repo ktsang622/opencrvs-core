757a5478e526576771c0b0655dc408bcbe0fdea7
Kevin Tsang <mail@ktsang.com>
2025-08-12 11:48:50 +0100

    fix opensearch port in toppan-service

--- packages/toppan-service/start.sh
+++ packages/toppan-service/start.sh
@@ -9,7 +9,7 @@ export TOPPAN_DB_PORT=${TOPPAN_DB_PORT:-5432}
 export TOPPAN_DB_NAME=${TOPPAN_DB_NAME:-person_registry}
 export TOPPAN_DB_USER=${TOPPAN_DB_USER:-registry_user}
 export TOPPAN_DB_PASSWORD=${TOPPAN_DB_PASSWORD:-registry_pass}
-export OSHOST=${OSHOST:-http://localhost:9200}
+export OSHOST=${OSHOST:-http://localhost:19200}
 export OPENSEARCH_ADMIN_PASSWORD=${OPENSEARCH_ADMIN_PASSWORD:-WelcomeDemo1.23@}
 export PORT=${PORT:-3888}
 
